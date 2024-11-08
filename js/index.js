62f8edda02fae0cfc817091cb1448ab11fa4ac77
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-11-07 21:12:45 -0500

    Update HTML and JavaScript files

--- js/index.js
+++ js/index.js
@@ -1,31 +1,39 @@
-const items = document.getElementById('items')
-const templateCard = document.getElementById('template-card').content
-const fragment = document.createDocumentFragment( )
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetchData()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
 
 const fetchData = async () => {
-    try {
-        const res = await fetch('../json/api.json')
-        const data = await res.json()
-        //console.log(data)
-        showCards(data)
-     }
-    catch(error){
-        console.log(error)
-    }
-}
-const showCards = data => {
+  try {
+    const res = await fetch("../json/api.json");
+    const data = await res.json();
+    showCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-data.forEach(product => {
-    templateCard.querySelector('h5').textContent = product.title
-    templateCard.querySelector('p').textContent = product.precio
-    templateCard.querySelector('img').setAttribute("src", product.thumbnailUrl)
-    templateCard.querySelector('button').dataset.id = product.id
-    const clone = templateCard.cloneNode(true)
-    fragment.appendChild(clone)
-})
-items.appendChild(fragment)
-}
+const showCards = (data) => {
+  data.forEach((product) => {
+    templateCard.querySelector("h5").textContent = product.title;
+    templateCard.querySelector("p").textContent = product.precio;
+    templateCard.querySelector("img").setAttribute("src", product.thumbnailUrl);
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  items.appendChild(fragment);
+
+  const buttons = items.querySelectorAll("button");
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      Swal.fire({
+        title: `Estás intentando comprar ${data[index].title}`,
+        text: "Blackpink in your area",
+        icon: "success",
+      });
+    });
+  });
+};
